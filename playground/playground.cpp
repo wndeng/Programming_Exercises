b17c4dc2c2db6afe09ba9c242bbf996f36999b19
Will <wnd211@nyu.edu>
2020-10-17 15:03:15 -0700

    labu labu

--- playground/playground.cpp
+++ playground/playground.cpp
@@ -1,11 +1,69 @@
 #include "../util/util.h"
 
-using namespace std;
+template<class T>
+class my_priority_queue {
+public:
+	my_priority_queue() {}
+	
+	// decrease top key freq by 1, return key val
+	static int remove(int val) {
+		auto &[v, l]{data[0]};
+		decreaseKey(0);	
 
-#include<list>
+			
+	}
+	// increase frequence of val by 1.
+	static void add(int val, int i) {
+		if(!index.count(val)) {
+			index[val] = data.size();
+			data.emplace_back(val, {i});
+		} else {
+			int idx{index[val]};
+			auto &[_, l]{data[idx]};
+			l.emplace_back(i);
+			increaseKey(idx);
+		}
+	}
+
+private:
+	void increaseKey(int i) {
+		while(i && greaterThan(i, parent(i))) {
+			std::swap(data[i], data[j]);
+			i = parent(i);
+		}
+	}
+
+	void decreaseKey(int i) {
+		int idx{index[val]};
+		std::swap(i, data[data.size()-1]);
+		data.pop_back();
+		int [l, r]{children(i)};
+		while(l || r) {
+			int max_{l == 0 ? r : r == 0 ? l : greaterThan(l, r) ? l : r};
+			if(greaterThan(i, max_)) return;
+			else {
+				std::swap(data[i], data[max_]);
+				i = max_i;
+			}
+		}
+		
+	}
+
+	std::pair<int,int> children(int i) {
+		int a{2*i+1 < data.size() ? 0 : 2*i+1}, b{2*i+2 < data.size() ? 0 : 2*i+2};
+		return {a, b};
+	}
+
+	static inline int parent(i) {
+		return i == 0 ? 0 : (i-1)>>1;	
+	}
+
+	std::unordered_map<int,int> index;	
+	std::vector<std::pair<int,std::list<int>> data;
+};
 
 int main() {
-	vector<int> m = {1,2,3};
-	for_ind(i, m) print(m[i]);
-	for_all(m) print(e);
+	auto f = [](const auto& a, const auto& b) {return a < b;};
+	test<int> t(f);
+	std::cout << t.run(2, 3) << std::endl;
 }
--- util/util.h
+++ util/util.h
@@ -14,14 +14,12 @@
 #include <time.h>
 #include <stack>
 #include <list>
-
-using namespace std;
-
+#include <functional>
 
 // Auxillary Functions
 
 template <typename T> 
-ostream& operator<<(ostream& os, const vector<T>& v) 
+std::ostream& operator<<(std::ostream& os, const std::vector<T>& v) 
 { 
     os << "[ "; 
     for (const auto &n: v) os << n << " "; 
@@ -38,20 +36,9 @@ typedef unsigned long long int ull;
 typedef uint32_t uint;
 typedef long int l;
 typedef unsigned long int ul;
-typedef vector<int> vi;
-typedef vector<string> vs;
-typedef vector<vector<int>> vvi;
-typedef pair<int,int> pi;
-
-// Abbreviations
-
-#define push_back pb;
-#define push p;
-#define emplace_back eb;
-#define first f;
-#define second s;
-#define for_all(v) for(const auto &e: v)
-#define for_ind(i, v) for(int i = 0, size = v.size(); i < size; ++i)
-
+typedef std::vector<int> vi;
+typedef std::vector<std::string> vs;
+typedef std::vector<std::vector<int>> vvi;
+typedef std::pair<int,int> pi;
 
 #endif
